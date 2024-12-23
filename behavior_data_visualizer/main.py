b969c8909a16775a803cd23b0a4adb6cf8046016
HernandoMV <hernybiotec@gmail.com>
2024-12-23 16:54:59 +0100

    with mpl

--- behavior_data_visualizer/main.py
+++ behavior_data_visualizer/main.py
@@ -5,6 +5,9 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 from lecilab_behavior_analysis import df_transforms as dft
+from lecilab_behavior_analysis import figure_maker as fm
+import io
+import base64
 
 # Load the data
 outpath = "/mnt/c/Users/HMARTINEZ/LeCiLab/data"
@@ -20,22 +23,34 @@ app = dash.Dash(__name__)
 
 # Create the layout
 app.layout = dash.html.Div([
-    dash.html.H1("Behavior Data Visualization"),
-    dash.dcc.Checklist(
-        id='dropdown',
-        options=[{'label': key, 'value': key} for key in data_dict.keys()],
-        value=[list(data_dict.keys())[0]],
-        labelStyle={'display': 'block'}
-    ),
-    dash.dcc.Graph(id='graph')
+    dash.dcc.Tabs([
+        dash.dcc.Tab(label='Reactive', children=[
+            dash.dcc.Checklist(
+                id='checklist',
+                options=[{'label': key, 'value': key} for key in data_dict.keys()],
+                value=[list(data_dict.keys())[0]],
+                labelStyle={'display': 'block'}
+            ),
+            dash.dcc.Graph(id='graph')
+        ]),
+
+        dash.dcc.Tab(label='Reports', children=[
+            dash.dcc.Dropdown(
+                id='dropdown',
+                options=[{'label': key, 'value': key} for key in data_dict.keys()],
+                value=list(data_dict.keys())[0],
+                multi=False,
+            ),
+            dash.html.Img(id='dropdown-graph', src=''),
+        ]),
+    ])
 ])
 
 # Create the callback
 @app.callback(
     dash.dependencies.Output('graph', 'figure'),
-    [dash.dependencies.Input('dropdown', 'value')]
+    [dash.dependencies.Input('checklist', 'value')],
 )
-
 def update_figure(selected_value):
     # merge the datasets of the selected mice
     tdfs = []
@@ -46,6 +61,22 @@ def update_figure(selected_value):
     tdf = pd.concat(tdfs)
     fig = px.line(tdf, x='total_trial', y='performance_w', color='mouse_name')
     return fig
+
+@app.callback(
+    dash.dependencies.Output('dropdown-graph', component_property='src'),
+    [dash.dependencies.Input('dropdown', 'value')],
+)
+def update_dropdown_figure(selected_value):
+    df = data_dict[selected_value]
+    fig = fm.subject_progress_figure(df, selected_value)
+    return fig_to_uri(fig)
+
+def fig_to_uri(fig):
+    buf = io.BytesIO()
+    fig.savefig(buf, format='png')
+    # buf.seek(0)
+    fig_data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f'data:image/png;base64,{fig_data}'
 
 # Run the app
 if __name__ == '__main__':
